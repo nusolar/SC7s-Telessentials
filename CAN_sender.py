bc4b43905afa0477d96eeca3f48f69b5049ce346
Chibu O <56417244+ChibuO@users.noreply.github.com>
2023-04-02 13:40:29 -0500

    add buffered code

--- CAN_sender.py
+++ CAN_sender.py
@@ -10,25 +10,25 @@ from cantools.database.can.database import Database
 from cantools.typechecking import SignalDictType
 from digi.xbee.devices import XBeeDevice
 
-from definitions import PROJECT_ROOT
+from definitions import PROJECT_ROOT, BUFFERED_XBEE_MSG_END
 from row import Row
 from util import add_dbc_file
 import serial
 
 VIRTUAL_BUS_NAME = "virtbus"
 
-XBEE_PORT = "COM9"
+XBEE_PORT = "COM6"
 XBEE_BAUD_RATE = 9600
 REMOTE_NODE_ID = "Node"
 
-SERIAL_PORT = "COM6"
+SERIAL_PORT = "COM9"
 SERIAL_BAUD_RATE = 500000
 
-#xbee = XBeeDevice(XBEE_PORT, XBEE_BAUD_RATE)
-#xbee.open()
+xbee = XBeeDevice(XBEE_PORT, XBEE_BAUD_RATE)
+xbee.open()
 
-#remote = xbee.get_network().discover_device(REMOTE_NODE_ID)
-#assert remote is not None
+remote = xbee.get_network().discover_device(REMOTE_NODE_ID)
+assert remote is not None
 
 # Thread communication globals
 row_lock = Lock()
@@ -63,7 +63,7 @@ def get_packets(interface) -> iter:
     else:
         raise Exception('Invalid interface')
 
-def row_accumulator_worker2(bus: can.ThreadSafeBus):
+def row_accumulator_worker(bus: can.ThreadSafeBus):
     """
     Observes messages sent on the `bus` and accumulates them in a global row.
     """
@@ -75,19 +75,9 @@ def row_accumulator_worker2(bus: can.ThreadSafeBus):
             for k, v in decoded.items():
                 rows[i].signals[k].update(v)
 
-def row_accumulator_worker(bus: can.ThreadSafeBus):
-    """
-    Observes messages sent on the `bus` and accumulates them in a global row.
-    """
-    while True:
-        msg = bus.recv()
-        assert msg is not None
-        print(msg.data)
-        # i = next(i for i, r in enumerate(rows) if r.owns(msg, db))
-        # decoded = cast(SignalDictType, db.decode_message(msg.arbitration_id, msg.data))
-        # with row_lock:
-        #     for k, v in decoded.items():
-        #         rows[i].signals[k].update(v)
+def buffered_payload(payload: str, chunk_size: int = 256, terminator: str = BUFFERED_XBEE_MSG_END) -> list[str]:
+        payload += terminator
+        return [payload[i:i + chunk_size] for i in range(0, len(payload), chunk_size)]
 
 def sender_worker():
     """
@@ -99,14 +89,15 @@ def sender_worker():
             copied = deepcopy(rows)
         for row in copied:
             row.stamp()
-            if row.name == "MPPT_0x600":
-                print()
-                print(row.serialize())
-                #xbee.send_data(remote, row.serialize())
+            for chunk in buffered_payload(row.serialize()):
+                print(chunk, "\n")
+                xbee.send_data(remote, chunk)
+                #xbee.send_data(remote, row.serialize()) #only call serialize once, it clears the val
+
 
 if __name__ == "__main__":
     # Create a thread to read off the bus and maintain the rows
-    accumulator = Thread(target=row_accumulator_worker2,
+    accumulator = Thread(target=row_accumulator_worker,
                          args=(can.ThreadSafeBus(VIRTUAL_BUS_NAME, bustype='virtual'),),
                          daemon=True)
 
